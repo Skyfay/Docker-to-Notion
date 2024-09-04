4665fd367f840de71a9d11e3385f0ef36a938258
Manu <skyfay@skymail.one>
2024-09-04 19:25:18 +0200

    update every 5 min + env variable config update

--- src/config.py
+++ src/config.py
@@ -4,10 +4,30 @@ import logging
 class Config:
     def __init__(self) -> None:
         self.config = {}
-
+    # works with docker-compose env
     def get(self, key: str) -> str:
         value = os.getenv(key)
         if value is None:
             logging.error(f"Environment variable {key} not set")
             raise KeyError(f"Environment variable {key} not set")
         return value
+
+
+# Code with .env file
+#    def __init__(self) -> None:
+#        self.config = {}
+#
+#        self.load(os.path.join(os.path.dirname(__file__), "..", ".env"))
+#
+#    def load(self, path: str) -> None:
+#        try:
+#            with open(path, "r") as f:
+#                for line in f:
+#                    key, value = line.strip().split("=")
+#                    self.config[key] = value
+#        except FileNotFoundError:
+#            logging.error(f"File {path} not found")
+#            raise
+#
+#    def get(self, key: str) -> str:
+#        return self.config[key]
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 from container import Container
 from notion import Notion
+import time
 
 def main():
 
     # Tests
     # Container().get_remote_image_info(image_name="docker.io/vaultwarden/server:latest")
     # Container().get_local_image_digest(image_name="docker.io/vaultwarden/server:latest")
-
+    while True:
     # Worker
-    containers = Container().get_running_containers()
-    Notion().update_notion_database(containers)
+        containers = Container().get_running_containers()
+        Notion().update_notion_database(containers)
+        print("Update completed. Waiting for the next sync...")
+        time.sleep(300)
 
 
 if __name__ == "__main__":
