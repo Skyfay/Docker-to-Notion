819917c9913029d0bc9d5fe59978f61c02906bf7
Manu <skyfay@skymail.one>
2024-09-08 16:37:51 +0200

    added the hostname to docker-compose file

--- archive/main.py
+++
@@ -1,9 +0,0 @@
-from container import Container
-from notion import Notion
-
-def main():
-    containers = Container().get_running_containers()
-    Notion().update_notion_database(containers)
-
-if __name__ == "__main__":
-    main()
